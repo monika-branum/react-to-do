217d02f1714b5c4a8f041dfa9bb652ce2352be8d
Monika <moncobra@comcast.net>
2023-01-30 19:39:08 -0800

    imports added for Auth, Auth function completed

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -3,3 +3,13 @@ import './Auth.css';
 import { useState } from 'react';
 import { authUser } from '../../services/auth.js';
 import { useUser } from '../../context/UserContext';
+
+export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { type } = useParams();
+  const { user, setUser } = useUser();
+  if (user) {
+    return <Redirect to="/items" />;
+  }
+}
